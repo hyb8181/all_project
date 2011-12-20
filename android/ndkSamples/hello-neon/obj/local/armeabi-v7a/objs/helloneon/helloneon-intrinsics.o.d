4255d99dc69140d8b8e932bfeba992ab8e3e0016
hyb8181 <hyb8181@gmail.com>
2011-12-20 13:04:39 +0800

    review

--- android/ndkSamples/hello-neon/obj/local/armeabi-v7a/objs/helloneon/helloneon-intrinsics.o.d
+++
@@ -1,5 +0,0 @@
-/cygdrive/f/workspace/hello-neon/obj/local/armeabi-v7a/objs/helloneon/helloneon-intrinsics.o: \
- /cygdrive/f/workspace/hello-neon/jni/helloneon-intrinsics.c \
- /cygdrive/f/workspace/hello-neon/jni/helloneon-intrinsics.h
-
-/cygdrive/f/workspace/hello-neon/jni/helloneon-intrinsics.h:
